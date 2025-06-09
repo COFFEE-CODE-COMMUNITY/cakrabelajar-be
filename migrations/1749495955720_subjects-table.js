a114b16682a85a46014fd63d2d2b2eccb52368fb
rafi tanujaya <rafibussines@gmail.com>
2025-06-10 04:05:35 +0700

    feat(migration): add migration file to create subjects table

---
+++ migrations/1749495955720_subjects-table.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('subjects', {
+        id : { type: 'VARCHAR(255)', notNull: true , primaryKey: true},
+        name: { type: 'VARCHAR(32)', notNull: true, unique: true},
+        desciption : {type: 'VARCHAR(255)' }
+    })
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable('subjects')
+};
